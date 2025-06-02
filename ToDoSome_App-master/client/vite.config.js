1fcb1a41fca8807eb573f58edb1126de514e3c71
Sesilia Nadine Puspa Dewi <sesilianadine@gmail.com>
2025-06-02 16:29:09 +0700

    initial push

---
+++ ToDoSome_App-master/client/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      proxy: {
+        "/service": {
+          target: mode === "development"
+            ? "http://localhost:5000"
+            : "https://e2425-wads-project-server.csbihub.id",
+          changeOrigin: true,
+          secure: mode !== "development",
+          ws: true,
+        },
+      },
+    },
+    build: {
+      outDir: "dist",
+    },
+    plugins: [react(), tailwindcss()],
+  }
+})
